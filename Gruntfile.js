7b50092854b30e5167ff55640eed73d3545a01e7
Sam Beveridge <sam.bev87@gmail.com>
2014-03-12 20:18:48 -0600

    Got sass working and compiling.  Linters are working. Created bower.json, package.json and Gemfile for dependency installs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
     var glob = {
-        scss: [
-            'assets/scss/*.scss'
-        ]
+        sass: [
+            'assets/sass/*.scss'
+        ],
     };
 
     grunt.initConfig({
@@ -15,13 +15,13 @@ module.exports = function(grunt) {
                     style: 'compressed'
                 },
                 files: {
-                    'assets/css/base.css': 'assets/scss/base.scss',
+                    'assets/css/base.css': 'assets/sass/base.scss',
                 }
             }
         },
 
         scsslint: {
-            allFiles: ['assets/scss/*.scss'],
+            allFiles: glob.sass,
             options: {
                 config: 'config/scss-lint.yml',
                 reporterOutput: 'build/lint/scss-lint-report.xml'
@@ -30,20 +30,17 @@ module.exports = function(grunt) {
 
         watch: {
             scss: {
-                files: glob.scss,
+                files: glob.sass,
                 tasks: ['sass', 'quality']
-            },
-            pep8: {
-                files: glob.python,
-                tasks: ['shell:pep8']
             }
         }
     });
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('scss', ['scss']);
-    grunt.registerTask('quality', ['scsslint', 'shell:pep8']);
+    grunt.registerTask('sass', ['sass']);
+    grunt.registerTask('quality', ['scsslint']);
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-scss-lint');
+}
